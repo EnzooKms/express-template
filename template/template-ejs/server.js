9bdc346dff55a42813b775b4c14990b8100e54a8
EnzooKms <ekemmas@edenschool.fr>
2023-06-18 22:50:30 +0200

    Prettier

--- template/template-ejs/server.js
+++ template/template-ejs/server.js
@@ -1,28 +1,26 @@
-const fs = require('fs')
-const express = require('express')
-const cors = require('cors')
-require('dotenv').config()
-const app = express()
-const port = process.env.PORT
+const fs = require("fs");
+const express = require("express");
+const cors = require("cors");
+require("colors");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT;
 
+fs.readdirSync("./routers").forEach(async (folder) => {
+  fs.readdirSync(`./routers/${folder}`).forEach(async (file) => {
+    const { router: route } = require(`./routers/${folder}/${file}`);
+    app.use(route);
+  });
+});
 
-fs.readdirSync('./routers').forEach(async f => {
-
-    fs.readdirSync(`./routers/${f}`).forEach(async file => {
-        const { router: route } = require(`./routers/${f}/${file}`)
-        app.use(route)
-    })
-
-})
-
-app.set('views', './resources/views')
-app.set('view engine', 'ejs')
-
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(express.static(process.cwd()))
+app.set("views", "./resources/views");
+app.set("view engine", "ejs");
+  
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(process.cwd()));
 
 app.listen(port, () => {
-    console.log(`Server running on port : ${port}`);
-})
+  console.log(`Server running on port ${port.magenta}`);
+});
